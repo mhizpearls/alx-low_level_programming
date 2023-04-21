6ff0e3f79953f2cc9b3e254df2f1715e0316028c
mhizpearls <esandrauwaila@gmail.com>
2023-04-21 16:10:04 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,13 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 48; num <= 57; num++)
 	{
 		putchar(num);
+		if (num == 57)
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
 	}
