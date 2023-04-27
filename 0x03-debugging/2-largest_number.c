05874ae362cace197217196d6f072302b4723af3
mhizpearls <esandrauwaila@gmail.com>
2023-04-27 04:33:35 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,17 @@ int largest_number(int a, int b, int c)
  {
  largest = a;
  }
- else if (a > b && c > a)
- {
- largest = c;
- }
- else if (b > c)
+ else if (b > c && b > a)
  {
  largest = b;
  }
- else
+ else if (c > b)
  {
  largest = c;
  }
-return(largest);
+ else
+ {
+ largest = b;
+ }
+return (largest);
 }
